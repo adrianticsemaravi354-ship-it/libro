a9822b2fc5aa81d900217896b450c5169747a1e1
adrianticsemaravi354-ship-it <adrianticsemaravi354@gmail.com>
2026-05-11 10:31:20 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,9 @@
+class Concatenacion {
+public static void main(String[] args) {
+String nombre = "Adrian";
+String primerApellido = "Ticse";
+String nombreConApellido =
+nombre + "" + primerApellido;
+System.out.println(nombreConApellido);
+} 
+} 
